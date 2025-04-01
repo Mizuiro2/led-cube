03bb5aefe54080b10f22a9fa84817ccd937fa992
水色の記憶 <115365112+Mizuiro2@users.noreply.github.com>
2025-04-01 17:34:20 +0800

    code0to9

---
+++ CUBE/CUBE_0to9/CUBE_0to9.ino
@@ -0,0 +1,61 @@
+#include <Adafruit_NeoPixel.h>
+#define NUM_LEDS 512
+#define LED_PIN 11
+
+Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+// 定义0-9的5x7点阵数据
+const uint8_t numbers[][7] = {
+    {0x1F, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1F}, // 0
+    {0x04, 0x0C, 0x04, 0x04, 0x04, 0x04, 0x0E}, // 1
+    {0x1F, 0x01, 0x01, 0x1F, 0x10, 0x10, 0x1F}, // 2
+    {0x1F, 0x01, 0x01, 0x1F, 0x01, 0x01, 0x1F}, // 3
+    {0x11, 0x11, 0x11, 0x1F, 0x01, 0x01, 0x01}, // 4
+    {0x1F, 0x10, 0x10, 0x1F, 0x01, 0x01, 0x1F}, // 5
+    {0x1F, 0x10, 0x10, 0x1F, 0x11, 0x11, 0x1F}, // 6
+    {0x1F, 0x01, 0x02, 0x04, 0x08, 0x08, 0x08}, // 7
+    {0x1F, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x1F}, // 8
+    {0x1F, 0x11, 0x11, 0x1F, 0x01, 0x01, 0x1F}  // 9
+};
+
+void setup() {
+    pixels.begin();
+}
+// ... 其他代码保持不变 ...
+
+void displayNumber(int num, uint32_t color) {
+    pixels.clear();
+    
+    int startX = 6;
+    int startY = 20;
+    
+    for (int row = 0; row < 7; row++) {
+        for (int col = 0; col < 5; col++) {
+            if (numbers[num][row] & (0x10 >> col)) {
+                int pixelIndex;
+                int y = startY + row;
+                int x = startX + col;
+                
+                // 修正扫描方向：奇数行从右到左，偶数行从左到右
+                if (y % 2 == 0) {
+                    pixelIndex = y * 16 + (15 - x);
+                } else {
+                    pixelIndex = y * 16 + x;
+                }
+                
+                pixels.setPixelColor(pixelIndex, color);
+            }
+        }
+    }
+    pixels.show();
+}
+
+// ... 其他代码保持不变 ...
+void loop() {
+    // 循环显示0-9
+    for (int i = 0; i < 10; i++) {
+        uint32_t color = pixels.Color(255, 102, 153);
+        displayNumber(i, color);
+        delay(1000);  // 每个数字显示1秒
+    }
+}
